834cb07c05f42462439d9b302e12bcf76629f19a
Nikodem Bochnacki <nikodem.bochnacki@iaisa.com>
2024-11-10 18:51:19 +0100

    added parentnode with recursive, unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,8 +1,8 @@
-class HTMLnode():
+class HTMLNode():
     def __init__(self, tag=None, value=None, children=None, props=None):
-        if children == None:
+        if children is None:
             children = []
-        if props == None:
+        if props is None:
             props = {}
         self.tag = tag
         self.value = value
@@ -16,6 +16,38 @@ class HTMLnode():
         x = ""
         for key, value in self.props.items():
             x += f" {key}='{value}'"
+        return x
 
     def __repr__(self):
         return f"HTMLNode(tag={self.tag!r}, value={self.value!r}, children={self.children!r}, props={self.props!r})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, children=[], props=props)
+        if self.value is None:
+            raise ValueError("You passed an empty value.")
+
+    def to_html(self):
+        if self.tag is None:
+            return self.value
+        s = ''
+        s += f"<{self.tag}"
+        s += self.props_to_html()
+        s += f">{self.value}</{self.tag}>"
+        return s
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children):
+        super().__init__(tag, children=children)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag has to be passed")
+        if self.children is None or self.children == []:
+            raise ValueError("There are no children associated")
+        s = f'<{self.tag}>'
+        
+        for child in self.children:
+            s += f"{child.to_html()}"
+        s += f'</{self.tag}>'
+        return s
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,14 +1,65 @@
 import unittest
-from htmlnode import HTMLnode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = HTMLnode("a", "https://google.pl/", None, {"href": "https://google.pl/", "target": "_blank",})
-        node2 = HTMLnode("p", "Typical paragraph of text", [node], {"color": "navy", "text-indent": "30px"})
-        # node3 = HTMLnode()
+###
+# html node'y
+###
+        node = HTMLNode("a", "https://google.pl/", None, {"href": "https://google.pl/", "target": "_blank"})
+        node2 = HTMLNode("p", "Typical paragraph of text", [node], {"color": "navy", "text-indent": "30px"})
+
         assert node.__repr__() == "HTMLNode(tag='a', value='https://google.pl/', children=[], props={'href': 'https://google.pl/', 'target': '_blank'})"
         assert node2.__repr__() == "HTMLNode(tag='p', value='Typical paragraph of text', children=[HTMLNode(tag='a', value='https://google.pl/', children=[], props={'href': 'https://google.pl/', 'target': '_blank'})], props={'color': 'navy', 'text-indent': '30px'})"
         self.assertNotEqual(node.__repr__(), node2.__repr__())
+
+###
+# leaf node'y
+###
+        leaf_node = LeafNode("a", "https://google.pl/", {"href": "https://google.pl/", "target": "_blank"})
+        leaf_node2 = LeafNode("p", "Typical paragraph of text.", {"color": "white"})
+        leaf_node3 = LeafNode("b", "", {})
+        leaf_node4 = LeafNode("", " ", {})
+        leaf_node5 = LeafNode(None, " ", {})
+
+        ## sprawdzenie wartościdrukowaniem
+        ## print(f"Expected leaf_node: ValueError('You passed an empty value.')")
+        ## print(f"Actual leaf_node: {leaf_node5.to_html()}")
+        ## print(f"")
+
+        assert leaf_node.to_html() == "<a href='https://google.pl/' target='_blank'>https://google.pl/</a>"
+        assert leaf_node2.to_html() == "<p color='white'>Typical paragraph of text.</p>"
+        assert leaf_node3.to_html() == "<b></b>"
+        assert leaf_node4.to_html() == "<> </>"
+        assert leaf_node5.to_html() == " "
+        try:
+            leaf_node6 = LeafNode("p", None, {})
+            assert False, "Value should have been passed"
+        except ValueError:
+            assert True
+
+### 
+# parent node'y
+###
+        parent_node = ParentNode("div", [LeafNode("b", "hello")])
+        grandparent_node = ParentNode("div", [ParentNode("p", [LeafNode(None, "Text of a grandchildren")])])
+        grandparent_node1 = ParentNode("div", [
+            ParentNode("ul", [LeafNode("i", "Typical leaf value", {"color": "blue"})]), 
+            ParentNode("ul", [LeafNode("b", "Second typical leaf value", {"color": "white"})])])
+
+        print(f"Actual: {grandparent_node1.to_html()}")
+        print(f"Expected: <div><ul><i color='blue'>Typical leaf value</i></ul><ul><b color='white'>Second typical leaf value</b></ul>")
+
+        assert parent_node.to_html() == "<div><b>hello</b></div>"
+        assert grandparent_node.to_html() == "<div><p>Text of a grandchildren</p></div>"
+        assert grandparent_node1.to_html() == "<div><ul><i color='blue'>Typical leaf value</i></ul><ul><b color='white'>Second typical leaf value</b></ul></div>"
+        try:
+            parent_node_exception = ParentNode("div", [
+                ParentNode("li", [LeafNode("b", None, {'color': 'black'})])
+            ])
+            assert False, "Value should have been passed"
+        except ValueError:
+            assert True
 
 if __name__ == "__main__":
     unittest.main()
