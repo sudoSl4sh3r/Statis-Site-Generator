b49ca910d6fd17e3ddec48938b932544e480fb19
Nikodem Bochnacki <nikodem.bochnacki@iaisa.com>
2024-11-16 14:58:47 +0100

    main.py changes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import re
+import re, os, shutil
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
 
@@ -26,7 +26,36 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
+def deleting_public_contents():
+    if os.path.exists("./public"):
+        contents = os.listdir(path="./public")
+        for item in contents:
+            item_path = os.path.join("./public", item)
+            if os.path.isfile(item_path):
+                os.remove(item_path)
+            elif os.path.isdir(item_path):
+                shutil.rmtree(item_path)
+    return "Deleted /public contents."
+
+def copy_directory(src, dest):
+    if not os.path.exists(dest):
+        os.mkdir(dest, mode=0o777)
+    for item in os.listdir(src):
+        item_path = os.path.join(src, item)
+        dest_path = os.path.join(dest, item)
+        if os.path.isdir(item_path):
+            copy_directory(item_path, dest_path)
+        else:
+            shutil.copy(item_path, dest_path)
+
+def copying_static_to_public():
+    print(deleting_public_contents())
+    print("Copying the /static contents to /public...")
+    copy_directory("./static", "./public")
+    print("Copying completed successfully.")
+
 def main():
+    copying_static_to_public()
     return
 
 main()
